75812321999f8600dae146695634b1adfacc99a6
Harish <hsnandi2002@gmail.com>
2024-06-05 12:06:09 +0530

    commit2

---
+++ accessspecifier/src/com/gvacharya/accessspecifier/AccessSpecifier.java
@@ -0,0 +1,25 @@
+package com.gvacharya.accessspecifier;
+
+public class AccessSpecifier {
+public static void main(String[] args) {
+	
+
+	/*
+	private int variable;
+	private TestAccessSpecifier() {
+		
+	}
+	
+	private void getVariable() {
+		return variable ;
+		}
+	
+	private void setVariable(int newVariable) {
+		getVariable1();
+		variable = newVariable;
+	}
+}
+*/
+
+}
+}
---
+++ accessspecifier/src/com/gvacharya/accessspecifier/TestAccessSpecifier.java
@@ -0,0 +1,7 @@
+package com.gvacharya.accessspecifier;
+
+public class TestAccessSpecifier {
+ public void accessSetVariableMethod() {
+	// TestAccessSpecifier obj1 = new TestAccessSpecifier
+ }
+}
