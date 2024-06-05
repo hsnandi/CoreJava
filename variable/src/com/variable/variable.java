128730fa11d4aa43d81f13357e10ce1695adc035
Harish <hsnandi2002@gmail.com>
2024-06-05 12:11:50 +0530

    commit 9

---
+++ variable/src/com/variable/variable.java
@@ -0,0 +1,21 @@
+package com.variable;
+
+public class variable {
+
+//Valid variable
+	int variable1;
+	int $variable;
+	int _variable;
+	int variableName;
+	  // Variables should always start with a small character or dollar and underscore. No other special character or number can be used($ and _ )
+
+//Invalid variable
+	/* int Variable;
+	   int *variable;
+	   int A;
+	   int 1variable;
+	   int variable name;
+	*/
+	
+	        //It shouldn't be a single character.
+}
